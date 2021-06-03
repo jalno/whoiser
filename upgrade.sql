f9d448ce1a2edcb16809040a1f5e72203dc4830c
Hossein Hosni <hosni.hossein@gmail.com>
2021-06-03 18:38:42 +0430

    Update upgrade.sql

--- upgrade.sql
+++ upgrade.sql
@@ -12,3 +12,15 @@ CREATE TABLE `whoiser_proxies` (
 	PRIMARY KEY (`id`),
 	KEY `ip` (`ip`,`port`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+--	Commit: 41283eea618c2d26aa9c72a7d017b2a001ee3448
+--
+ALTER TABLE `whoiser_domains`
+	CHANGE `name` `name` VARCHAR(127) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+	CHANGE `tld` `tld` VARCHAR(63) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+	CHANGE `domain` `domain` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+	CHANGE `statuses` `statuses` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+	CHANGE `registrar` `registrar` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+	CHANGE `nservers` `nservers` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL; 
+
